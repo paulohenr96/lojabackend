4f00cd967bef1c0a81a2001f3b28673fc2ce6373
paulohenr96 <paulohrq_96@hotmail.com>
2024-02-04 19:25:50 -0300

    ProductControllerTest completed

--- estudandoconfig/src/main/java/com/paulo/estudandoconfig/dto/ProductDTO.java
+++ estudandoconfig/src/main/java/com/paulo/estudandoconfig/dto/ProductDTO.java
@@ -8,36 +8,50 @@ public class ProductDTO {
 	private Integer quantity;
 	private BigDecimal price;
 	private String category;
+
 	public String getName() {
 		return name;
 	}
+
 	public ProductDTO setName(String name) {
-		this.name = name;return this;
+		this.name = name;
+		return this;
 	}
+
 	public Integer getQuantity() {
 		return quantity;
 	}
+
 	public ProductDTO setQuantity(Integer quantity) {
-		this.quantity = quantity;return this;
+		this.quantity = quantity;
+		return this;
 	}
+
 	public BigDecimal getPrice() {
 		return price;
 	}
+
 	public ProductDTO setPrice(BigDecimal price) {
-		this.price = price;return this;
+		this.price = price;
+		return this;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public ProductDTO setId(Long id) {
-		this.id = id;return this;
+		this.id = id;
+		return this;
 	}
+
 	public String getCategory() {
 		return category;
 	}
+
 	public ProductDTO setCategory(String category) {
-		this.category = category;return this;
+		this.category = category;
+		return this;
 	}
-	
-	
+
 }
--- estudandoconfig/src/test/java/com/paulo/estudandoconfig/controller/ProductControllerTest.java
+++ estudandoconfig/src/test/java/com/paulo/estudandoconfig/controller/ProductControllerTest.java
@@ -1,24 +1,218 @@
 package com.paulo.estudandoconfig.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.paulo.estudandoconfig.dto.InfoDTO;
+import com.paulo.estudandoconfig.dto.ProductDTO;
 import com.paulo.estudandoconfig.service.ProductService;
-import com.paulo.estudandoconfig.service.SaleService;
-@WebMvcTest(controllers = SaleController.class)
+
+@WebMvcTest(controllers = ProductController.class)
 class ProductControllerTest {
 	@MockBean
 	ProductService service;
 
 	@Test
-	void saveProductSuccessful() {
+	void saveProductSuccessful() throws JsonProcessingException, Exception {
+		ProductController controller = new ProductController(service);
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		String url = "/products";
 
+		ProductDTO dto = new ProductDTO().setName("Socks").setPrice(BigDecimal.valueOf(9000)).setCategory("clothe")
+				.setQuantity(90);
+		when(service.save(dto)).thenReturn("");
+
+		// Act
+//		when(userMapper.toEntity(user)).
+
+		MvcResult result = mockMvc.perform(post(url).contentType(MediaType.APPLICATION_JSON_VALUE).content(toJson(dto)))
+				.andReturn();
+		assertEquals(200, result.getResponse().getStatus());
+		assertEquals("", result.getResponse().getContentAsString());
+
+	}
+
+	@Test
+	void getAllSuccessful() throws JsonProcessingException, Exception {
+		ProductController controller = new ProductController(service);
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		String url = "/products";
+
+		ProductDTO dto = new ProductDTO().setName("Socks").setPrice(BigDecimal.valueOf(9000)).setCategory("clothe")
+				.setQuantity(90);
+
+		when(service.getAll(any(PageRequest.class))).thenReturn(new PageImpl<>(List.of(dto)));
+
+		// Act
+//		when(userMapper.toEntity(user)).
+
+		mockMvc.perform(get(url)).andExpect(jsonPath("$.content[0].name").value("Socks")).andExpect(status().isOk());
+
+	}
+
+	@Test
+	void getAllByCategorySuccessful() throws JsonProcessingException, Exception {
+		ProductController controller = new ProductController(service);
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		String url = "/products";
+		String category = "clothes";
+		ProductDTO dto = new ProductDTO().setName("Socks").setPrice(BigDecimal.valueOf(9000)).setCategory("clothes")
+				.setQuantity(90);
+
+		when(service.getAll(any(PageRequest.class), anyString())).thenReturn(new PageImpl<>(List.of(dto)));
+
+		// Act
+//		when(userMapper.toEntity(user)).
+
+		mockMvc.perform(get(url.concat("/category/".concat(category))))
+				.andExpect(jsonPath("$.content[0].name").value("Socks"))
+				.andExpect(jsonPath("$.content[0].category").value(category)).andExpect(status().isOk());
+
+	}
+
+	@Test
+	void updateSuccessful() throws JsonProcessingException, Exception {
+		ProductController controller = new ProductController(service);
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 		
+		String url = "/products";
+		
+		Long id = 1L;
+		String category = "clothes";
+		
+		ProductDTO dto = new ProductDTO().setName("Socks")
+
+				.setPrice(BigDecimal.valueOf(9000)).setCategory("clothes").setQuantity(90);
+
+		when(service.updateById(any(ProductDTO.class), anyLong())).thenReturn(dto);
+
+
+		mockMvc.perform(put(url+"/"+id)
+				.contentType(MediaType.APPLICATION_JSON_VALUE).content(toJson(dto)))
+				
+				.andExpect(status().isOk()).andExpect(jsonPath("$.name").value("Socks"));
+
+	}
+	@Test
+	void deteleSuccessful() throws JsonProcessingException, Exception {
+		ProductController controller = new ProductController(service);
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		
+		String url = "/products";
+		
+		Long id = 1L;
+		String category = "clothes";
+		
+		ProductDTO dto = new ProductDTO().setName("Socks")
+
+				.setPrice(BigDecimal.valueOf(9000)).setCategory("clothes").setQuantity(90);
+
+		when(service.deleteById(anyLong())).thenReturn(new ResponseEntity<>(HttpStatus.OK));
+
+
+		mockMvc.perform(delete(url+"/"+id))
+				
+				.andExpect(status().isOk());
+
+	}
+	@Test
+	void findByIdSuccessful() throws JsonProcessingException, Exception {
+		ProductController controller = new ProductController(service);
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		
+		String url = "/products";
+		
+		Long id = 1L;
+		String category = "clothes";
+		
+		ProductDTO dto = new ProductDTO().setName("Socks")
+
+				.setPrice(BigDecimal.valueOf(9000)).setCategory("clothes").setQuantity(90);
+
+		when(service.findById(anyLong())).thenReturn(dto);
+
+
+		mockMvc.perform(get(url+"/"+id))
+				
+				
+				.andExpect(status().isOk()).andExpect(jsonPath("$.name").value("Socks"));
+
+	}
+	
+	@Test
+	void countProductSuccessful() throws JsonProcessingException, Exception {
+		ProductController controller = new ProductController(service);
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		
+		String url = "/products";
+		
+		Long id = 1L;
+		String category = "clothes";
+		
+		ProductDTO dto = new ProductDTO().setName("Socks")
+
+				.setPrice(BigDecimal.valueOf(9000)).setCategory("clothes").setQuantity(90);
+		;
+		when(service.info()).thenReturn(new InfoDTO(BigDecimal.valueOf(400),500L,50));
+
+
+		mockMvc.perform(get(url+"/infos"))
+				
+				
+				.andExpect(status().isOk()).andExpect(jsonPath("$.income").value("400"));
+
+	}
+	@Test
+	void checkQuantitySuccessful() throws JsonProcessingException, Exception {
+		ProductController controller = new ProductController(service);
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		
+		String url = "/products";
+		
+		Long id = 1L;
+		String category = "clothes";
+		
+		ProductDTO dto = new ProductDTO().setName("Socks")
+
+				.setPrice(BigDecimal.valueOf(9000)).setCategory("clothes").setQuantity(90);
+		;
+		when(service.checkQuantity()).thenReturn(List.of(dto));
+
+
+		mockMvc.perform(get(url+"/checkquantity"))
+				
+				
+				.andExpect(status().isOk()).andExpect(jsonPath("$.[0].name").value("Socks"));
+
+	}
+	String toJson(Object o) throws JsonProcessingException {
+		return new ObjectMapper().writeValueAsString(o);
 	}
 
 }

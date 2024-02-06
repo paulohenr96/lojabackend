0397e7ad08dd1f83c41e7743dd7d54e84e773d6d
paulohenr96 <paulohrq_96@hotmail.com>
2024-02-06 17:32:35 -0300

    Handling the method exception from the spring validator

--- estudandoconfig/src/main/java/com/paulo/estudandoconfig/exception/ExcentionController.java
+++ estudandoconfig/src/main/java/com/paulo/estudandoconfig/exception/ExcentionController.java
@@ -1,7 +1,14 @@
 package com.paulo.estudandoconfig.exception;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +26,28 @@ public class ExcentionController {
 	@ResponseBody
 	public ResponseEntity<String> handleException(RuntimeException e) {
 		e.printStackTrace();
-		return new ResponseEntity<String>("Internal Server Error",HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<String>(e.getMessage(),HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ResponseBody
+	public ResponseEntity<String> handleException(IllegalArgumentException e) {
+		e.printStackTrace();
+		return new ResponseEntity<String>(e.getMessage(),HttpStatus.BAD_REQUEST);
+	}
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<List<String>> handleValidationExceptions(
+	  MethodArgumentNotValidException ex) {
+	    List<String> erros=new ArrayList<>();
+	    ex.getBindingResult().getAllErrors().forEach((error) -> {
+	        String fieldName = ((FieldError) error).getField();
+	        String errorMessage = error.getDefaultMessage();
+	       erros.add(errorMessage);
+	    });
+		return new ResponseEntity<List<String>>(erros,HttpStatus.BAD_REQUEST);
+
 	}
 }

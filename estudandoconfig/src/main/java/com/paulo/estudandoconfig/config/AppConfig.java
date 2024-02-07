56d59aa2c6028edaf1cbd81aca2a1f26108e8ef3
paulohenr96 <paulohrq_96@hotmail.com>
2024-02-07 14:09:46 -0300

    Beans that are going to read the messages.proproperties

--- estudandoconfig/src/main/java/com/paulo/estudandoconfig/config/AppConfig.java
+++ estudandoconfig/src/main/java/com/paulo/estudandoconfig/config/AppConfig.java
@@ -1,14 +1,16 @@
 package com.paulo.estudandoconfig.config;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import com.paulo.estudandoconfig.JWTCreator;
-import com.paulo.estudandoconfig.repository.SaleRepository;
-import com.paulo.estudandoconfig.service.ProductService;
 import com.paulo.estudandoconfig.service.SaleService;
+
+import jakarta.validation.Validator;
 
 @Configuration
 public class AppConfig {
@@ -23,17 +25,31 @@ public class AppConfig {
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
-	
+
 	@Bean
 	public SaleService saleService() {
 		return new SaleService();
 	}
+
 	@Bean
 	public JWTCreator JWTCreator() {
 		return new JWTCreator();
 	}
-	
-	
-	
-	
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages");
+
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+	@Bean
+	public LocalValidatorFactoryBean validator() {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource());
+		return bean;
+	}
+
 }
